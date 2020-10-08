a59dbdf88f4ea82b2b8284b445b21ea652e0755b
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-08 21:01:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
 
   def execute_transaction
     if self.status != "complete"
-      if self.valid?
+      if sender.valid? && receiver.valid?
         sender.balance -= amount
         receiver.balance += amount
         self.status = "complete"
