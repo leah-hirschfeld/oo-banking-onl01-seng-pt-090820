2588e187c50e52edff19f0d76e9c276ec9c0d730
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-08 20:47:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,8 @@ class Transfer
   def execute_transaction
     # BankAccount.balance
     if sender.valid? && receiver.valid?
-      sender.balance += amount
-      receiver.balance -= amount
+      sender.balance -= amount
+      receiver.balance += amount
       @status = "complete"
     else
       @status = "rejected"
