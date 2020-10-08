109e68686ba0b31080daad804c9e5c91f6c64239
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-08 21:00:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,25 +15,16 @@ class Transfer
 
   def execute_transaction
     if self.status != "complete"
-      if self.valid?
-      sender.balance = sender.balance - amount
-      receiver.balance = receiver.balance + amount
-      self.status = "complete"
+      if sender.valid? && receiver.valid?
+        sender.balance -= amount
+        receiver.balance += amount
+        self.status = "complete"
+      else
+        self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
       end
     end
   end
-      
-      
-      #if sender.valid? && receiver.valid?
-        #sender.balance -= amount
-        #receiver.balance += amount
-        #self.status = "complete"
-      #else
-        #self.status = "rejected"
-        #"Transaction rejected. Please check your account balance."
-      #end
-    #end
-  #end
   
   def reverse_transfer
     #return sender.amount from Transfer to sender.balance BankAccount
